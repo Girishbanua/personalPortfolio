80ddc84e39721a570059fe25670c0420019a882e
Girish Chandra banua <111453017+Girishbanua@users.noreply.github.com>
2025-04-02 19:54:11 +0530

    files added

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {      
+      colors: {
+        "themeOrange": "",
+        "darkBg": "#121212",
+        "rich-black": "#00111cff",
+        "rich-black-2": "#001523ff",
+        "rich-black-3": "#001a2cff",
+        "prussian-blue": "#002137ff",
+        "prussian-blue-2": "#00253eff",
+        "prussian-blue-3": "#002945ff",
+        "prussian-blue-4": "#002e4eff",
+        "prussian-blue-5": "#003356ff",
+        "indigo-dye": "#003a61ff",
+        "indigo-dye-2": "#00406cff",        
+      },
+      fontFamily:{
+        "styled-font": "LXGW WenKai TC, cursive",
+        "elegant": "Montserrat, sans-serif"
+      },
+      backgroundImage:{
+        "bgLight": "linear-gradient(to bottom right, #FFECD2 40%, #FCB69F 100%)",        
+      }
+    },
+  },
+  plugins: [],  
+};
